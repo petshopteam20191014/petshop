1d05c019637cdc157fe077a5cebcc7d5e81e0150
Administrator <Administrator@TH6CPLTXOIMEYUR>
2019-10-16 18:53:26 +0800

    为个为个

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -11,73 +11,38 @@ $(function(){
 		$(this).hide();
 	})
 	//鼠标触碰展开详细的内容
-	$(".mainNav01>ul>li:eq(0)").hover(function(){
-		$(".mainNav03:eq(0)").show();
-	},function(){
-		$(".mainNav03:eq(0)").hide();
-	})
-	$(".mainNav01>ul>li:eq(1)").hover(function(){
-		$(".mainNav03:eq(1)").show();
-	},function(){
-		$(".mainNav03:eq(1)").hide();
-	})
-	$(".mainNav01>ul>li:eq(2)").hover(function(){
-		$(".mainNav03:eq(2)").show();
-	},function(){
-		$(".mainNav03:eq(2)").hide();
-	})
-	$(".mainNav01>ul>li:eq(3)").hover(function(){
-		$(".mainNav03:eq(3)").show();
-	},function(){
-		$(".mainNav03:eq(3)").hide();
-	})
-	$(".mainNav01>ul>li:eq(4)").hover(function(){
-		$(".mainNav03:eq(4)").show();
-	},function(){
-		$(".mainNav03:eq(4)").hide();
-	})
-	$(".mainNav01>ul>li:eq(5)").hover(function(){
-		$(".mainNav03:eq(5)").show();
-	},function(){
-		$(".mainNav03:eq(5)").hide();
-	})
-	$(".mainNav01>ul>li:eq(6)").hover(function(){
-		$(".mainNav03:eq(6)").show();
-	},function(){
-		$(".mainNav03:eq(6)").hide();
-	})
-	$(".mainNav01>ul>li:eq(7)").hover(function(){
-		$(".mainNav03:eq(7)").show();
-	},function(){
-		$(".mainNav03:eq(7)").hide();
-	})
-	$(".mainNav01>ul>li:eq(8)").hover(function(){
-		$(".mainNav03:eq(8)").show();
-	},function(){
-		$(".mainNav03:eq(8)").hide();
-	})
+	for (var n = 0;n < 9; n ++) {
+		$(".mainNav01>ul>li:eq("+n+")").bind("mouseover",{id:n},function(event){
+			$(".mainNav03:eq("+event.data.id+")").show();
+			$(".mainNav02").bind("mouseover",{id:event.data.id},function(event){
+				$(".mainNav03").hide();
+				$(".mainNav03:eq("+event.data.id+")").show();
+			}).bind("mouseout",{id:n},function(event){
+				$(".mainNav03").hide();
+			})
+		}).bind("mouseout",{id:n},function(event){
+			$(".mainNav03:eq("+event.data.id+")").hide();
+		})
+	}
 	
-	$(".mainNav02").bind("mouseover",function(){
-		console.log(111);
-		$(".mainNav01").show();	
-	})
-	$("#header .mainNav01 li").bind("mouseover",function(){
-		$(this).css({"border-color":"chocolate",
-					"border-right": "2px solid ghostwhite",
-					"border-left": "2px solid coral"});
-		
-			
-	})
-	$("#header .mainNav01 li").bind("mouseover",function(){
-		console.log(111);
-		$(this).children("a").css({"font-size": "14px",
-					"color": "coral"});
-	})
-	
-	
-	
-	
-	
+	//当鼠标触碰左侧导航栏是的动作
+	 $("#header .mainNav01 li").bind("mouseover",function(){
+		 $(this).addClass("mainNavCss");
+		 
+		//当触碰详细栏时，左侧栏不消失，切出现的切口不消失；
+	 		$(".mainNav02").bind("mouseover",{id:this},function(event){
+	 			$(".mainNav01").show();	
+	 			$("#header .mainNav01 li").removeClass("mainNavCss");
+	 			 $(event.data.id).addClass("mainNavCss");
+	 		}).bind("mouseout",{id:this},function(event){
+	 			$(".mainNav01").hide();	
+	 			 $(event.data.id).removeClass("mainNavCss");
+	 		});
+	 }).bind("mouseout",function(){
+		 $(this).removeClass("mainNavCss");
+	 })
+	 
+
 //	$.ajax({
 //		type:"post",
 //		url:"index",
