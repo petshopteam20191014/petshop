5ebc54c7713e8ef75b8b114533aa974b456f910d
Administrator <Administrator@TH6CPLTXOIMEYUR>
2019-10-18 08:39:11 +0800

    11121212

--- src/main/resources/static/js/NavView.js
+++ src/main/resources/static/js/NavView.js
@@ -20,7 +20,7 @@ $(function(){
 	//获取页面跳转的是那种分类，然后加载对应的图片
 	
 	var picId = index.substring(1,2);
-	console.log(picId);
+//	console.log(picId);
 	if (picId==2){
 		$("#content .backImg div:eq(0)").css("background-image","url(img/1554373195017694390.jpg)")
 		.next().css("background-image","url(img/1554373248885044037.jpg)");
@@ -78,11 +78,13 @@ $(function(){
 			var $proview = $(".productView>div:eq(2)>ul");
 			var $rexiao = $(".rexiao")
 			for (var x in data){
+				var pdid =data[x].pdid;
+//				console.log($pdid);
 				$proview.append('<li>'
 								+	'<ul>'
 								+		'<li><img src="img/product/'+data[x].picture[0].pictureName+'" ></li>'
-								+		'<li><a href="#" style="color:#1B1B15">'+data[x].pdname+'</a></li>'
-								+		'<li><a href="#" style="color: #666666;">品牌样式:'+data[x].brand.bname+'</a></li>'
+								+		'<li><a href="productView.html?'+pdid+'" style="color:#1B1B15">'+data[x].pdname+'</a></li>'
+								+		'<li><a href="productView.html?'+pdid+'" style="color: #666666;">品牌样式:'+data[x].brand.bname+'</a></li>'
 								+		'<li>&yen;&nbsp;'+data[x].cost+'</li>'
 								+		'<li><a href="#" style="color: white">加入购物车</a></li>'
 								+	'</ul>'
---
+++ src/main/resources/static/js/productView.js
@@ -0,0 +1,36 @@
+$(function(){
+	//获取url地址的参数
+	var url= window.location.href;
+	var index = url.substring(url.lastIndexOf('?') + 1);
+	console.log(index);
+	
+	//左侧显示图片的缩略图
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	$.ajax({
+//		type:"post",
+//		url:"index",
+//		data:{},
+//		dataType:"json",
+//		success:function(data){
+//			console.log(data);
+//			
+//		}
+//		
+//	})	
+})
