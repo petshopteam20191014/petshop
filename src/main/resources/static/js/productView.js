3ac59b19dcef848ab0b41fadc7ba750f48faeee2
Administrator <Administrator@TH6CPLTXOIMEYUR>
2019-10-19 09:33:05 +0800

    实现图片局部放大显示

--- src/main/resources/static/js/productView.js
+++ src/main/resources/static/js/productView.js
@@ -1,11 +1,11 @@
 $(function(){
 	//获取url地址的参数
-//	var url= window.location.href;
-//	var index = url.substring(url.lastIndexOf('?') + 1);
+	var url= window.location.href;
+	var indexurl = url.substring(url.lastIndexOf('?') + 1);
 //	console.log(index);
 	
 	//假设一个测试值
-	var index = "2107A22112".substring(0,5);
+	var index = indexurl.substring(0,5);
 //	console.log(index);
 //	console.log(2);
 	//左侧显示图片的缩略图
@@ -14,16 +14,20 @@ $(function(){
 	$.ajax({
 		type:"post",
 		url:"selectProductByPdid",
-		data:{"pdid":"2107A"},
+		data:{"pdid":index},
 		dataType:"json",
 		success:function(data){
 //			console.log(data);
-			$(".leftView>div:eq(1)").append('<img src="img/product/'+data[0].picture[0].pictureName+'" >');	
+			$(".leftView>div:eq(1)").append('<img src="img/product/'+data[0].picture[0].pictureName+'" jqimg="img/product/'+data[0].picture[0].pictureName+'">');	
 			//左侧缩略图
 			for (var n = 0; n < 4; n ++){
 //				console.log(n);
-				$(".leftView01>div:eq(1) li:eq("+n+")").append('<img src="img/product/'+data[0].picture[n].pictureName+'" >');
+				$(".leftView01>div:eq(1) li:eq("+n+")").append('<img src="img/product/'+data[0].picture[n].pictureName+'" jqimg="img/product/'+data[0].picture[n].pictureName+'">');
 			}
+			//初始化该页面的商品信息
+			$(".rightView li:eq(0)").text(data[0].pdname);
+			$(".rightView li:eq(1) span").text(data[0].cost);
+			
 		}
 	})
 	//获取该商品的图片个数,同时计算出页数
@@ -31,7 +35,7 @@ $(function(){
 	$.ajax({
 		type:"post",
 		url:"selectPicNum",
-		data:{"pictureId":"2107A"},
+		data:{"pictureId":index},
 		dataType:"json",
 		success:function(data){
 //			console.log(Math.ceil(data.result/4));
@@ -51,7 +55,7 @@ $(function(){
 		$.ajax({
 			type:"post",
 			url:"selectProductByPicId",
-			data:{"pictureId":"2107A","startP":startP,"endP":endP},
+			data:{"pictureId":index,"startP":startP,"endP":endP},
 			dataType:"json",
 			success:function(data){
 //				console.log(data);
@@ -60,7 +64,7 @@ $(function(){
 				for (var n = 0; n < 4; n ++){
 //					console.log(n);
 					if(data[n] != null){
-						$(".leftView01>div:eq(1) li:eq("+n+")").append('<img src="img/product/'+data[n].pictureName+'" >');
+						$(".leftView01>div:eq(1) li:eq("+n+")").append('<img src="img/product/'+data[n].pictureName+'" jqimg="img/product/'+data[n].pictureName+'">');
 					}
 					
 				}
@@ -71,7 +75,7 @@ $(function(){
 	//下一页
 	picNum.then(function(count){
 		$downPic.bind("click",function(){
-			if (startP < count-1){
+			if (startP < count-4){
 				startP ++;
 				endP ++;
 				addPic();
@@ -90,6 +94,12 @@ $(function(){
 		
 	})
 	
+	$(".leftView01>div:eq(1) li").bind("click",function(){
+//		console.log($(this).children());
+		$(".leftView>div:eq(1) img").remove();
+		$(this).children().clone().appendTo(".leftView>div:eq(1) ");
+//		$(".leftView>div:eq(1) ").append($(this).children());
+	})
 	
 	
 	
